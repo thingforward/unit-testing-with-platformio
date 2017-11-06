15c31d548c8d1a44a0df639cff3cf2b47541b34f
Dominik Fehr <dominik.fehr@aleri.de>
2017-11-06 13:22:07 +0100

    initial advanced scripting test

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,32 @@
-#ifndef UNIT_TEST
-#ifdef ARDUINO
-
+#ifdef ESP8266
 #include <Arduino.h>
-
-/* need to include the module here */
-#include "mod1.h"
-
-void setup() {
-}
-
-void loop() {
-}
-
 #else
-int main(int argc, char **argv) {
-}
+#include <stdlib.h>
 #endif
-#endif
+
+void f(void) {
+  //cppcheck test
+  char a[10];
+  a[10] = 0;
+
+  //valgrind test
+  malloc(1024);
+ }
+
+ #ifdef ESP8266
+ void setup() {
+   Serial.begin(9600);
+
+   f();
+ }
+
+ void loop() {
+   printf("%s\n", "hello world");
+ }
+
+ #else
+ int main() {
+   f();
+   return 0;
+ }
+ #endif
